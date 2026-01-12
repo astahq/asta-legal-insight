40b8fccfd4bc89c25514d326c1cffe102c45a329
onurhan1337 <onurhandtr@gmail.com>
2026-01-12 22:23:30 +0300

    refactor: simplify worker script to directly fetch assets without error handling

--- worker.ts
+++ worker.ts
@@ -6,28 +6,6 @@ interface Env {
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
-    try {
-      const url = new URL(request.url);
-      const pathname = url.pathname;
-
-      const hasExtension = /\.\w+$/.test(pathname);
-
-      if (hasExtension || pathname.startsWith("/assets/")) {
-        const response = await env.ASSETS.fetch(request);
-        if (response.status === 404) {
-          return new Response("Not Found", { status: 404 });
-        }
-        return response;
-      }
-
-      const indexRequest = new Request(new URL("/index.html", url.origin), {
-        method: request.method,
-        headers: request.headers,
-      });
-
-      return env.ASSETS.fetch(indexRequest);
-    } catch (error) {
-      return new Response("Internal Server Error", { status: 500 });
-    }
+    return env.ASSETS.fetch(request);
   },
 };
